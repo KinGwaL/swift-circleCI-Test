83f8ec22803c64ad4885c44afdb9fbd5cb5faf50
KING <>
2019-06-18 11:22:49 +0800

    no message

--- Game/AppDelegate.swift
+++ Game/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        for _ in 0...1 {
+            let a = UILabel()
+        }
+        
         return true
     }
 
